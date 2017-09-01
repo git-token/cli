82ae7c4b2c5e1b5712dc06d51f4705c6a613ac01
Ryan Tate <ryan.michael.tate@gmail.com>
2017-08-31 22:21:05 -0700

    WIP

--- index-signer.js
+++ index-signer.js
@@ -6,6 +6,7 @@ program
 	.option('-i, --init', 'Initialize New GitToken Signer')
   .option('-s, --socket <SocketPath>', 'Signer Socket Path')
   .action(function(opts) {
+		console.log('opts', opts)
     opts.forEach(function(opt) {
       console.log('opt', opt)
     })
