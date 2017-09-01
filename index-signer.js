7e31ee57916cd2d2ca27cc3fb3fabc11f885a582
Ryan Tate <ryan.michael.tate@gmail.com>
2017-08-31 22:37:09 -0700

    WIP

--- index-signer.js
+++ index-signer.js
@@ -5,12 +5,12 @@ const program = require('commander');
 program
 	.option('-i, --init', 'Initialize New GitToken Signer')
   .option('-s, --socket <SocketPath>', 'Signer Socket Path')
-  .action(function(socket, opts) {
-		console.log('socket', socket)
-		console.log('opts', opts)
-    opts.forEach(function(opt) {
-      console.log('opt', opt)
-    })
-  });
+  .action(function(option, args) {
+  	if(args.init) {
+			console.log('Init new GitToken Signer')
+		} else if(args.socket) {
+			console.log('socket path:', args.socket)
+		}
+	});
 
 program.parse(process.argv);
