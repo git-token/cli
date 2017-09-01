f96ab7c7e8ebcae0913370cb5c2705fe4d1bba4b
Ryan Tate <ryan.michael.tate@gmail.com>
2017-09-01 00:46:26 -0700

    Adding env to args

--- index-signer.js
+++ index-signer.js
@@ -3,31 +3,55 @@
 const program = require('commander');
 const GitTokenSigner = require('gittoken-signer').default;
 
+
+
 program
-	.option('-i, --init', 'Initialize New GitToken Signer')
+  .option('-i, --init', 'Initialize New GitToken Signer')
   .option('-s, --socket <Socket Path>', 'Signer Socket Path')
   .option('--web3 <Web3 Provider>', 'Ethereum Web3 Provider')
-	.option('-d --dir <Directory Path>', 'Directory to Save Keystore')
-	.parse(process.argv);
+  .option('-d --dir <Directory Path>', 'Directory to Save Keystore')
+  .option('-e --env <Environmental Vairable Path>', 'Environmental Path')
+  .parse(process.argv);
 
-if (!program.web3) {
-	console.log('Web3 Provider Missing! Exiting...')
-	process.exit(1)
+let signerIpcPath, recover, dirPath, web3Provider;
+
+if (!program.env) {
+  if (!program.web3) {
+    console.log('Web3 Provider Missing! Exiting...')
+    process.exit(1)
+  } else if (!program.dir) {
+    console.log('Directory Path Missing! Exiting...')
+    process.exit(1)
+  } else if (!program.socket) {
+    console.log('Socket Path Missing! Exiting...')
+    process.exit(1)
+  } else {
+    recover = program.init ? false : true;
+    dirPath = program.dir;
+    web3Provider = program.web3;
+    signerIpcPath = program.socket;
+  }
+} else {
+  console.log(`
+    Looking for an .env file with the following varaibles:
+
+      RECOVER_KEYSTORE
+      KEYSTORE_DIR_PATH
+      WEB3_PROVIDER
+      SIGNER_IPC_PATH
+  `)
+
+  require('dotenv').config({ path: program.env })
+  recover = process.env['RECOVER_KEYSTORE'] == 'true' ? true : false;
+  dirPath = process.env['KEYSTORE_DIR_PATH'];
+  web3Provider = process.env['WEB3_PROVIDER'];
+  signerIpcPath = process.env['SIGNER_IPC_PATH'];
 }
 
-if (!program.dir) {
-  console.log('Directory Path Missing! Exiting...')
-  process.exit(1)
-}
-
-if (!program.socket) {
-  console.log('Socket Path Missing! Exiting...')
-  process.exit(1)
-}
-
+// Run the signer
 const signer = new GitTokenSigner({
-	signerIpcPath: program.socket,
-	recover: program.init != true,
-	dirPath: program.dir,
-	web3Provider: program.web3
+  signerIpcPath,
+  recover,
+  dirPath,
+  web3Provider
 })
