656fe9c88a905beb86042062c2709e9c015a8e03
Ryan Tate <ryan.michael.tate@gmail.com>
2017-09-12 23:16:28 -0700

    WIP

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ const program = require('commander');
 program
   .version(require('../package.json').version)
   .command('setup', 'Setup GitToken Docker Host')
-	.command('configure', 'Configure GitToken Docker Services')
+	.command('register', 'Register GitHub organization with https://registry.gittoken.io')
   .command('deploy', 'Deploy GitToken Docker Services')
 	.parse(process.argv);
