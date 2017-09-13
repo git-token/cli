8c35af769451a2e9ff2ce3f092fe8dda234b65b3
Ryan Tate <ryan.michael.tate@gmail.com>
2017-09-12 23:16:52 -0700

    WIP

--- dist/index.js
+++ dist/index.js
@@ -3,4 +3,4 @@
 
 var program = require('commander');
 
-program.version(require('../package.json').version).command('setup', 'Setup GitToken Docker Host').command('configure', 'Configure GitToken Docker Services').command('deploy', 'Deploy GitToken Docker Services').parse(process.argv);
+program.version(require('../package.json').version).command('setup', 'Setup GitToken Docker Host').command('register', 'Register GitHub organization with https://registry.gittoken.io').command('deploy', 'Deploy GitToken Docker Services').parse(process.argv);
