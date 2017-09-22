52aa51ef795025295fe8a71b4426e5126e5c8176
Ryan Tate <ryan.michael.tate@gmail.com>
2017-09-22 11:43:09 -0700

    Temporarily commenting out setup and deploy command options; WIP

--- dist/index.js
+++ dist/index.js
@@ -3,4 +3,8 @@
 
 var program = require('commander');
 
-program.version(require('../package.json').version).command('setup', 'Setup GitToken Docker Host').command('register', 'Register GitHub organization with https://registry.gittoken.io').command('deploy', 'Deploy GitToken Docker Services').parse(process.argv);
+program.version(require('../package.json').version)
+// .command('setup', 'Setup GitToken Docker Host')
+.command('register', 'Register GitHub organization with https://registry.gittoken.io')
+// .command('deploy', 'Deploy GitToken Docker Services')
+.parse(process.argv);
